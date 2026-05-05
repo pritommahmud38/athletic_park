e6624b0aad6b397f3abcf753128e032c54597cb6
pritommahmud38 <1902038@icte.bdu.ac.bd>
2026-05-05 23:50:35 +0600

    Added screenshots

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $currency = '৳';
 $db_username = 'root';
 $db_password = '';
-$db_name = 'athletic_park';
+$db_name = 'athleticpark';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
